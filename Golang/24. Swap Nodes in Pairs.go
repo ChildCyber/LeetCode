0a278dcae430023b99a95288c7a16ac4433ab115
Mr.Child <lishunsheng2012@gmail.com>
2022-02-27 22:55:27 +0800

    update comment

--- Golang/24. Swap Nodes in Pairs.go
+++ Golang/24. Swap Nodes in Pairs.go
@@ -2,11 +2,14 @@ package leetcode
 
 // 两两交换链表中的节点
 // https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+// 两两交换链表中相邻的节点，并返回交换后链表的头节点
+
 // 迭代
 func swapPairs(head *ListNode) *ListNode {
 	dummyHead := &ListNode{Next: head}
-	temp := dummyHead // 头
+	temp := dummyHead // 表示当前到达的节点
 	for temp.Next != nil && temp.Next.Next != nil {
+		// 每次交换 temp 后面的两个节点
 		node1 := temp.Next
 		node2 := temp.Next.Next
 		// 交换节点位置，从头到尾，node1和node2位置互换，temp的next指针指向node2
@@ -20,12 +23,19 @@ func swapPairs(head *ListNode) *ListNode {
 }
 
 // 递归
-func swapPairs1(head *ListNode) *ListNode {
+func swapPairsRec(head *ListNode) *ListNode {
+	// 递归的终止条件是链表中没有节点，或者链表中只有一个节点
 	if head == nil || head.Next == nil {
 		return head
 	}
-	newHead := head.Next
-	head.Next = swapPairs(newHead.Next)
-	newHead.Next = head
-	return newHead
+
+	// 链表中至少有两个节点
+	// 原始链表的头节点变成新的链表的第二个节点，原始链表的第二个节点变成新的链表的头节点，链表中的其余节点的两两交换可以递归地实现
+	one := head
+	two := one.Next
+	three := two.Next
+
+	two.Next = one
+	one.Next = swapPairsRec(three)
+	return two
 }
--- Golang/739. Daily Temperatures.go
+++ Golang/739. Daily Temperatures.go
@@ -7,7 +7,7 @@ func dailyTemperatures(temperatures []int) []int {
 	ans, j := make([]int, len(temperatures)), 0
 	for i := 0; i < len(temperatures); i++ {
 		for j = i + 1; j < len(temperatures); j++ {
-			if temperatures[j] > temperatures[i] {
+			if temperatures[j] > temperatures[i] { // 找到第一个更大的元素
 				ans[i] = j - i
 				break
 			}
@@ -22,15 +22,18 @@ func dailyTemperatures1(temperatures []int) []int {
 	ans := make([]int, length)
 	stack := []int{}
 
-	// 维护一个存储下标的单调栈，从栈底到栈顶的下标对应的温度列表中的温度依次递减。如果一个下标在单调栈里，则表示尚未找到下一次温度更高的下标。
+	// 遍历每日温度，维护一个存储下标的单调栈，从栈底到栈顶的下标对应的温度列表中的温度依次递减。
+	// 如果一个下标在单调栈里，则表示尚未找到下一次温度更高的下标。
 	for i := 0; i < length; i++ {
 		temperature := temperatures[i]
+		// 当日温度大于栈顶温度，说明栈顶元素升温日已经找到，将栈顶元素出栈
 		for len(stack) > 0 && temperature > temperatures[stack[len(stack)-1]] {
 			prevIndex := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			ans[prevIndex] = i - prevIndex
 		}
-		stack = append(stack, i)
+		// 若栈为空或者当日温度小于等于栈顶温度则直接入栈
+		stack = append(stack, i) // 存储下标
 	}
 	return ans
 }
