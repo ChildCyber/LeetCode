7d0b034c4ecff77c21fc07c286e85fe168821c9d
Mr.Child <lishunsheng2012@gmail.com>
2022-02-28 23:06:16 +0800

    update comment

--- Golang/1143. Longest Common Subsequence.go
+++ Golang/1143. Longest Common Subsequence.go
@@ -5,6 +5,10 @@ package leetcode
 // 动态规划
 func longestCommonSubsequence(text1, text2 string) int {
 	m, n := len(text1), len(text2)
+	// dp[i][j] 表示 text1[0:i] 和 text2[0:j] 的最长公共子序列的长度
+	// 边界条件：
+	// 当 i=0 时，text1[0:i] 为空，dp[0][j]=0
+	// 当 j=0 时，text2[0:j] 为空，dp[i][0]=0
 	dp := make([][]int, m+1) // (m+1)*(n+1)
 	for i := range dp {
 		dp[i] = make([]int, n+1)
@@ -12,9 +16,9 @@ func longestCommonSubsequence(text1, text2 string) int {
 
 	for i, c1 := range text1 {
 		for j, c2 := range text2 {
-			if c1 == c2 {
+			if c1 == c2 { // 两字符相同
 				dp[i+1][j+1] = dp[i][j] + 1
-			} else {
+			} else { // 两字符不同
 				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
 			}
 		}
--- Golang/209. Minimum Size Subarray Sum.go
+++ Golang/209. Minimum Size Subarray Sum.go
@@ -32,22 +32,26 @@ func minSubArrayLen(s int, nums []int) int {
 
 // 滑动窗口
 func minSubArrayLen1(s int, nums []int) int {
+	// 思路：每一轮迭代，将 nums[end]加到 sum，
+	// 如果 sum≥s，则更新子数组的最小长度（此时子数组的长度是end−start+1），
+	// 然后将 nums[start] 从 sum 中减去并将 start 右移，直到 sum<s，在此过程中同样更新子数组的最小长度。
+	// 在每一轮迭代的最后，将 end 右移。
 	n := len(nums)
 	if n == 0 {
 		return 0
 	}
 
 	ans := math.MaxInt32
-	start, end := 0, 0
-	sum := 0
-	for end < n { // 小于，不可越界
+	start, end := 0, 0 // 滑动窗口的开始位置和结束位置
+	sum := 0           // 从nums[start]到nums[end]的子数组中的元素和
+	for end < n {      // 小于，不可越界
 		sum += nums[end]
 		for sum >= s { // 大于等于，start右移
 			ans = min(ans, end-start+1)
 			sum -= nums[start]
 			start++
 		}
-		end++
+		end++ // end右移
 	}
 	if ans == math.MaxInt32 {
 		return 0
