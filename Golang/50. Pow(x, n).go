5e845e9092c1d489ff3a700d18bee923dc4122df
Mr.Child <lishunsheng2012@gmail.com>
2022-03-06 23:31:08 +0800

    add 50

---
+++ Golang/50. Pow(x, n).go
@@ -0,0 +1,22 @@
+package leetcode
+
+// Pow(x, n)
+// https://leetcode-cn.com/problems/powx-n
+// 快速幂 + 递归
+func myPow(x float64, n int) float64 {
+	if n >= 0 {
+		return quickMul(x, n)
+	}
+	return 1.0 / quickMul(x, -n)
+}
+
+func quickMul(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	y := quickMul(x, n/2)
+	if n%2 == 0 {
+		return y * y
+	}
+	return y * y * x
+}
