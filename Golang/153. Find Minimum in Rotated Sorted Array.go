22126ea80d73ef3235948dc90fe5143a8beed144
Mr.Child <lishunsheng2012@gmail.com>
2022-03-08 22:41:49 +0800

    add 153

---
+++ Golang/153. Find Minimum in Rotated Sorted Array.go
@@ -0,0 +1,19 @@
+package leetcode
+
+// 寻找旋转排序数组中的最小值
+// https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+// 遍历，O(n)
+
+// 二分，O(log n)
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		mid := low + (high-low)/2
+		if nums[mid] < nums[high] {
+			high = mid
+		} else {
+			low = mid + 1
+		}
+	}
+	return nums[low]
+}
