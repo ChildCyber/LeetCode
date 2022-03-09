cf27690d0d19ad95a42c31fcb3ec8b7758851169
Mr.Child <lishunsheng2012@gmail.com>
2022-03-09 23:28:33 +0800

    add 402

---
+++ Golang/402. Remove K Digits.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "strings"
+
+// 移掉K位数字
+// https://leetcode-cn.com/problems/remove-k-digits
+// 贪心 + 单调栈
+func removeKdigits(num string, k int) string {
+	stack := []byte{}
+	for i := range num {
+		digit := num[i]
+		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, digit)
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		ans = "0"
+	}
+	return ans
+}
