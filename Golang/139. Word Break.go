7b8cfc2e5a792def33b5c030bdcbe379ca1fe68e
Mr.Child <lishunsheng2012@gmail.com>
2022-03-17 21:11:48 +0800

    add 139

---
+++ Golang/139. Word Break.go
@@ -0,0 +1,23 @@
+package leetcode
+
+// 单词拆分
+// https://leetcode-cn.com/problems/word-break/
+// 动态规划
+func wordBreak(s string, wordDict []string) bool {
+	wordDictSet := make(map[string]bool)
+	for _, w := range wordDict {
+		wordDictSet[w] = true
+	}
+	// dp[i] 表示字符串 s 前 i 个字符组成的字符串 s[0..i−1] 是否能被空格拆分成若干个字典中出现的单词
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && wordDictSet[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
