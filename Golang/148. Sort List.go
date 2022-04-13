fcbc4f67c2da2406ba4cbe63a80c79e5adbf5927
Mr.Child <lishunsheng2012@gmail.com>
2022-04-13 21:40:47 +0800

    update comment

--- Golang/148. Sort List.go
+++ Golang/148. Sort List.go
@@ -3,10 +3,7 @@ package leetcode
 // 排序链表
 // https://leetcode-cn.com/problems/sort-list/
 // 自顶向下归并排序
-func merge148(head1, head2 *ListNode) *ListNode {
-	// 1. 找到链表的中点，以中点为分界，将链表拆分成两个子链表。寻找链表的中点可以使用快慢指针的做法，快指针每次移动 2 步，慢指针每次移动 1 步，当快指针到达链表末尾时，慢指针指向的链表节点即为链表的中点。
-	// 2. 对两个子链表分别排序。
-	// 3. 将两个排序后的子链表合并，得到完整的排序后的链表。
+func merge148(head1, head2 *ListNode) *ListNode { // 合并两个排序后的子链表
 	dummyHead := &ListNode{}
 	temp, temp1, temp2 := dummyHead, head1, head2
 	for temp1 != nil && temp2 != nil {
@@ -27,11 +24,16 @@ func merge148(head1, head2 *ListNode) *ListNode {
 	return dummyHead.Next
 }
 
-func sort148(head, tail *ListNode) *ListNode {
+func sort148(head, tail *ListNode) *ListNode { // 递归
+	// 1. 找到链表的中点，以中点为分界，将链表拆分成两个子链表。寻找链表的中点可以使用快慢指针的做法，快指针每次移动 2 步，慢指针每次移动 1 步，当快指针到达链表末尾时，慢指针指向的链表节点即为链表的中点。
+	// 2. 对两个子链表分别排序。
+	// 3. 将两个排序后的子链表合并，得到完整的排序后的链表。
+
+	// 递归的终止条件是链表的节点个数小于或等于 1
+	// 当链表为空或者链表只包含 1 个节点时，不需要对链表进行拆分和排序。
 	if head == nil {
 		return head
 	}
-
 	if head.Next == tail {
 		head.Next = nil
 		return head
--- Golang/151. Reverse Words in a String.go
+++ Golang/151. Reverse Words in a String.go
@@ -5,9 +5,9 @@ import "strings"
 // 翻转字符串里的单词
 // https://leetcode-cn.com/problems/reverse-words-in-a-string
 func reverseWords151(s string) string {
-	ss := strings.Fields(s)
-	reverse151(&ss, 0, len(ss)-1)
-	return strings.Join(ss, " ")
+	ss := strings.Fields(s)       // 空格拆分字符串
+	reverse151(&ss, 0, len(ss)-1) // 翻转
+	return strings.Join(ss, " ")  // 连接
 }
 
 func reverse151(m *[]string, i int, j int) {
