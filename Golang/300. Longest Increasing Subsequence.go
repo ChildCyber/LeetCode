1045730ac5150477e6d03d968be370bd95dc7a81
Mr.Child <lishunsheng2012@gmail.com>
2022-04-17 21:43:32 +0800

    update comment

--- Golang/300. Longest Increasing Subsequence.go
+++ Golang/300. Longest Increasing Subsequence.go
@@ -3,6 +3,7 @@ package leetcode
 // 最长递增子序列
 // https://leetcode-cn.com/problems/longest-increasing-subsequence/
 // 动态规划
+// O(n^2)
 func lengthOfLIS(nums []int) int {
 	// dp[i] 代表 [0,i] 范围内,选择数字 nums[i] 为结尾可以获得的最⻓上升子序列的⻓度。
 	// 状态转移方程为 dp[i] = max(dp[j]) + 1 ,其中 j < i && nums[j] > nums[i] ,取所有满足条件的最大值。
@@ -14,7 +15,7 @@ func lengthOfLIS(nums []int) int {
 				dp[i] = max(dp[i], dp[j])
 			}
 		}
-		dp[i] = dp[i] + 1 // i == 1, dp[1]=1
+		dp[i] = dp[i] + 1 // dp[1]=1
 		res = max(res, dp[i])
 	}
 	return res
@@ -27,7 +28,7 @@ func lengthOfLIS1(nums []int) int {
 	}
 	dp := make([]int, length) // dp长度和原数组相同
 	for i := 0; i < length; i++ {
-		dp[i] = 1
+		dp[i] = 1                // 每一个数都有可能是最长子序列的起点
 		for j := 0; j < i; j++ { // j < i
 			if nums[i] > nums[j] {
 				dp[i] = max(dp[i], dp[j]+1)
@@ -36,4 +37,58 @@ func lengthOfLIS1(nums []int) int {
 		}
 	}
 	return ans
+}
+
+// 分治 + 递归
+// https://leetcode.cn/problems/longest-increasing-subsequence/solution/300zui-chang-di-zeng-zi-xu-lie-cong-bao-bwso6/
+func lengthOfLISForce(nums []int) int {
+	ans := 0
+
+	var dfs func(int, int) int
+	dfs = func(index int, last int) int {
+		if index == len(nums) {
+			return 1
+		}
+		length := dfs(index+1, last)
+		if nums[index] > last {
+			length = max(length, dfs(index+1, nums[index])+1)
+		}
+		return length
+	}
+
+	for i := 0; i < len(nums); i++ {
+		ans = max(ans, dfs(i, nums[i]))
+	}
+	return ans
+}
+
+// 暴力
+// 通过回溯获取数组的所有子序列，再对这些子串再依次判定是否为「严格上升」
+func lengthOfLISBacktrace(nums []int) int {
+	// dfs 遍历不知是否会超时 O(N*2^n) 超时了GG
+	stack := make([]int, 0, 16)
+	res := 0
+
+	var dfs func(nums []int, index int)
+	dfs = func(nums []int, index int) {
+		for i := index; i < len(nums); i++ {
+			if nums[i] <= stack[len(stack)-1] {
+				continue
+			}
+
+			// 要
+			stack = append(stack, nums[i])
+			dfs(nums, i+1)
+			//不要
+			stack = stack[:len(stack)-1]
+		}
+		res = max(res, len(stack))
+	}
+
+	for i := 0; i < len(nums); i++ {
+		stack = append(stack, nums[i])
+		dfs(nums, i+1)
+		stack = stack[:len(stack)-1]
+	}
+	return res
 }
