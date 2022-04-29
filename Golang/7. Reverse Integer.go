a8a5ba12dca2926010dd0ca12c3f734ffb733850
Mr.Child <lishunsheng2012@gmail.com>
2022-04-29 22:28:35 +0800

    update comment

--- Golang/7. Reverse Integer.go
+++ Golang/7. Reverse Integer.go
@@ -3,13 +3,14 @@ package leetcode
 // 整数反转
 // https://leetcode-cn.com/problems/reverse-integer
 func reverse7(x int) int {
-	var tmp int
+	var ans int
 	for x != 0 {
-		tmp = tmp*10 + x%10
+		ans = ans*10 + x%10
 		x /= 10
 	}
-	if tmp > 1<<31-1 || tmp < -(1<<31) {
+	// 范围在[−2^31, 2^31−1]
+	if ans > 1<<31-1 || ans < -(1<<31) {
 		return 0
 	}
-	return tmp
+	return ans
 }
--- Golang/9. Palindrome Number.go
+++ Golang/9. Palindrome Number.go
@@ -6,11 +6,13 @@ func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
 	}
-	// 反转数字，判断两数是否相等
+
+	// 反转数字
 	var tmp, rev = x, 0
 	for tmp != 0 {
 		rev = rev*10 + tmp%10
 		tmp /= 10
 	}
+	// 判断两数是否相等
 	return rev == x
 }
