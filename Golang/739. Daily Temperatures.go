0fba26125c26f6859fe3a1e9229bb34bf4c982ff
Mr.Child <lishunsheng2012@gmail.com>
2022-05-08 21:44:08 +0800

    update comment

--- Golang/739. Daily Temperatures.go
+++ Golang/739. Daily Temperatures.go
@@ -20,13 +20,13 @@ func dailyTemperatures(temperatures []int) []int {
 func dailyTemperatures1(temperatures []int) []int {
 	length := len(temperatures)
 	ans := make([]int, length)
-	stack := []int{}
+	stack := []int{} // 单调栈
 
-	// 遍历每日温度，维护一个存储下标的单调栈，从栈底到栈顶的下标对应的温度列表中的温度依次递减。
-	// 如果一个下标在单调栈里，则表示尚未找到下一次温度更高的下标。
+	// 遍历每日温度，维护一个存储下标的单调栈，从栈底到栈顶的下标对应的温度列表中的温度依次递减
+	// 如果一个下标在单调栈里，则表示尚未找到下一次温度更高的下标
 	for i := 0; i < length; i++ {
 		temperature := temperatures[i]
-		// 当日温度大于栈顶温度，说明栈顶元素升温日已经找到，将栈顶元素出栈
+		// 构建单调栈：当日温度大于栈顶温度，说明栈顶元素升温日已经找到，将栈顶元素出栈
 		for len(stack) > 0 && temperature > temperatures[stack[len(stack)-1]] {
 			prevIndex := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
--- Golang/78. Subsets.go
+++ Golang/78. Subsets.go
@@ -22,8 +22,29 @@ func generateSubsets(nums []int, k, start int, c []int, res *[][]int) {
 	for i := start; i < len(nums)-(k-len(c))+1; i++ {
 		c = append(c, nums[i])
 		generateSubsets(nums, k, i+1, c, res)
-		// 回溯
-		c = c[:len(c)-1]
+		c = c[:len(c)-1] // 回溯
 	}
+	return
+}
+
+// 回溯
+func subsets1(nums []int) (ans [][]int) {
+	set := []int{}
+
+	var dfs func(int)
+	dfs = func(cur int) {
+		if cur == len(nums) {
+			ans = append(ans, append([]int(nil), set...))
+			return
+		}
+		// 选
+		set = append(set, nums[cur])
+		dfs(cur + 1)
+		// 不选
+		set = set[:len(set)-1]
+		dfs(cur + 1)
+	}
+
+	dfs(0)
 	return
 }
