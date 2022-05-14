52774083c01ae945fcdcc408fec6112b7cc3608e
Mr.Child <lishunsheng2012@gmail.com>
2022-05-14 21:53:59 +0800

    update comment

--- Golang/46. Permutations.go
+++ Golang/46. Permutations.go
@@ -10,22 +10,19 @@ func permute(nums []int) [][]int { // 不含重复数字的数组
 		return [][]int{}
 	}
 
-	// 状态变量
 	// path:已经选了哪些数，数组
 	// depth:递归到第几层
 	// used布尔数组
-	// res:结果
-	used, path, res := make([]bool, len(nums)), []int{}, [][]int{}
-	generatePermutation(nums, 0, path, &res, &used)
-	return res
+	// ans:结果
+	used, path, ans := make([]bool, len(nums)), []int{}, [][]int{}
+	generatePermutation(nums, 0, path, &ans, &used)
+	return ans
 }
 
-func generatePermutation(nums []int, depth int /*递归深度*/, path []int, res *[][]int, used *[]bool) {
+func generatePermutation(nums []int, depth int /*递归深度*/, path []int, ans *[][]int, used *[]bool) {
 	// 当前递归层数等于数组长度，递归终止
 	if depth == len(nums) {
-		temp := make([]int, len(path))
-		copy(temp, path)
-		*res = append(*res, temp)
+		*ans = append(*ans, append([]int(nil), path...))
 		return
 	}
 	// 递归，考虑数组中所有的数
@@ -33,10 +30,10 @@ func generatePermutation(nums []int, depth int /*递归深度*/, path []int, res
 		if !(*used)[i] { // 当前数未被使用，使用该数继续进行回溯
 			(*used)[i] = true                                   // 标记为已使用
 			path = append(path, nums[i])                        // 当前数添加到path
-			generatePermutation(nums, depth+1, path, res, used) // 递归考虑下一层，只有depth需要变，表示递归到下一层
-			// 回溯部分，前面做了什么，就需要做对应的逆操作
+			generatePermutation(nums, depth+1, path, ans, used) // 递归考虑下一层，只有depth需要变，表示递归到下一层
+			// 回溯，前面做了什么，就需要做对应的逆操作
 			path = path[:len(path)-1] // 回到上一层节点
-			(*used)[i] = false        // 未使用
+			(*used)[i] = false        // 标记为未使用
 		}
 	}
 	return
