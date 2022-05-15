cecc6243ffe6f72127857eeb17237474739116a7
Mr.Child <lishunsheng2012@gmail.com>
2022-05-15 21:54:30 +0800

    update 19

--- Golang/19. Remove Nth Node From End of List.go
+++ Golang/19. Remove Nth Node From End of List.go
@@ -5,17 +5,31 @@ package leetcode
 // 快慢指针
 func removeNthFromEnd1(head *ListNode, n int) *ListNode {
 	// https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/solution/dong-hua-tu-jie-leetcode-di-19-hao-wen-ti-shan-chu/
-	dummyHead := &ListNode{Next: head}
-	preSlow, slow, fast := dummyHead, head, head
-	for fast != nil {
-		if n <= 0 { // fast & slow一起走
-			preSlow = slow
-			slow = slow.Next
-		}
-		// fast先走n次
-		n--
-		fast = fast.Next
+	dummy := &ListNode{Next: head}
+	first, second := head, dummy
+	for i := 0; i < n; i++ { // 先走n次
+		first = first.Next
 	}
-	preSlow.Next = slow.Next // 删除倒数第N个结点
-	return dummyHead.Next
+	for ; first != nil; first = first.Next {
+		second = second.Next
+	}
+	second.Next = second.Next.Next // 删除倒数第N个结点
+	return dummy.Next
+}
+
+// 计算链表长度
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	length := 0
+	cur := head
+	for ; head != nil; head = head.Next {
+		length++
+	}
+
+	dummy := &ListNode{0, head}
+	cur = dummy
+	for i := 0; i < length-n; i++ {
+		cur = cur.Next
+	}
+	cur.Next = cur.Next.Next
+	return dummy.Next
 }
