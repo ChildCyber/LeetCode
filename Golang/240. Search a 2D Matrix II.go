53a44a1fa9b5d0069f6709256a88e21e88c4ef4d
Mr.Child <lishunsheng2012@gmail.com>
2022-05-30 21:32:09 +0800

    update comment

--- Golang/240. Search a 2D Matrix II.go
+++ Golang/240. Search a 2D Matrix II.go
@@ -2,30 +2,26 @@ package leetcode
 
 // 搜索二维矩阵 II
 // https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
-// Z 字形查找
-func searchMatrix2(matrix [][]int, target int) bool {
-	if len(matrix) == 0 {
-		return false
-	}
-	row, col := 0, len(matrix[0])-1
-	for col >= 0 && row <= len(matrix)-1 { // 从矩阵 matrix 的右上角 (0,n−1) 进行搜索
-		if target == matrix[row][col] {
-			return true
-		}
 
-		if target > matrix[row][col] {
-			row++
-		} else {
-			col--
+// 直接查找
+func searchMatrix(matrix [][]int, target int) bool {
+	// 直接遍历整个矩阵matrix，判断target是否出现
+	for _, row := range matrix {
+		for _, v := range row {
+			if v == target {
+				return true
+			}
 		}
 	}
 	return false
 }
 
+// 二分查找
 func searchMatrix2BS(matrix [][]int, target int) bool {
 	if len(matrix) == 0 {
 		return false
 	}
+
 	for _, row := range matrix {
 		low, high := 0, len(matrix[0])
 		for low <= high {
@@ -39,5 +35,30 @@ func searchMatrix2BS(matrix [][]int, target int) bool {
 			}
 		}
 	}
+	return false
+}
+
+// Z 字形查找
+func searchMatrix2(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+
+	// 从矩阵 matrix 的右上角 (0,n−1) 进行搜索
+	row, col := 0, len(matrix[0])-1
+	for col >= 0 && row <= len(matrix)-1 {
+		if target == matrix[row][col] {
+			return true
+		}
+
+		// 在每一步的搜索过程中，如果位于位置 (x,y)，希望在以 matrix 的左下角为左下角、以 (x,y) 为右上角的矩阵中进行搜索
+		if target > matrix[row][col] { // 所有位于该列的元素都是严格大于target的
+			row++
+		} else { // 所有位于该行的元素都是严格小于target的
+			col--
+		}
+	}
+
+	// 超出矩阵边界，不存在target
 	return false
 }
--- Golang/338. Counting Bits.go
+++ Golang/338. Counting Bits.go
@@ -1,13 +1,26 @@
 package leetcode
 
+import "math/bits"
+
 // 比特位计数
 // https://leetcode-cn.com/problems/counting-bits/
 // 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
-// Golang bits.OnesCount计算给定整数的二进制表示中的1的数目
-func countBits(num int) []int { // dp，最低设置位
-	bits := make([]int, num+1)  // dp[0] = 0，0的比特位为0
-	for i := 1; i <= num; i++ { // 从1开始
-		bits[i] += bits[i&(i-1)] + 1
+
+// 内置函数
+func countBitsBuiltin(num int) []int {
+	ans := make([]int, num+1)
+	for i := 0; i <= num; i++ {
+		ans[i] = bits.OnesCount(uint(i))
 	}
-	return bits
+	return ans
+}
+
+// 动态规划，最低设置位
+func countBits(num int) []int {
+	ans := make([]int, num+1) // dp[0] = 0，0的比特位为0
+	// 遍历从 1 到 num 的每个正整数 i，计算 ans 的值。最终得到的数组 ans 即为答案
+	for i := 1; i <= num; i++ { // 由于是从0-num是递增序列，则每次使用 n &= n - 1去除一位1后，必定小于原数，则必能在以前序列中找到此数，将其原有的值加上去除的1可得当前数1的个数
+		ans[i] += ans[i&(i-1)] + 1
+	}
+	return ans
 }
