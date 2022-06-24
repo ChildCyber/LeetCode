b282005698207b5ec65d36796e48dafb6f799934
Mr.Child <lishunsheng2012@gmail.com>
2022-06-24 21:01:20 +0800

    add 73

---
+++ Golang/73. Set Matrix Zeroes.go
@@ -0,0 +1,72 @@
+package leetcode
+
+// 矩阵置零
+// https://leetcode.cn/problems/set-matrix-zeroes/
+// 使用标记数组
+func setZeroes(matrix [][]int) {
+	row := make([]bool, len(matrix))
+	col := make([]bool, len(matrix[0]))
+
+	for i, r := range matrix {
+		for j, v := range r {
+			if v == 0 {
+				row[i] = true
+				col[j] = true
+			}
+		}
+	}
+
+	for i, r := range matrix {
+		for j := range r {
+			if row[i] || col[j] {
+				r[j] = 0
+			}
+		}
+	}
+}
+
+// 使用两个标记变量
+func setZeroes1(matrix [][]int) {
+	n, m := len(matrix), len(matrix[0])
+	row0, col0 := false, false
+
+	for _, v := range matrix[0] {
+		if v == 0 {
+			row0 = true
+			break
+		}
+	}
+	for _, r := range matrix {
+		if r[0] == 0 {
+			col0 = true
+			break
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	if row0 {
+		for j := 0; j < m; j++ {
+			matrix[0][j] = 0
+		}
+	}
+	if col0 {
+		for _, r := range matrix {
+			r[0] = 0
+		}
+	}
+}
