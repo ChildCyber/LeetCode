9cf408e34cfb0e5e306cee8c18b450f8b1d11dd4
Mr.Child <lishunsheng2012@gmail.com>
2022-06-26 21:01:07 +0800

    add 29

---
+++ Golang/29. Divide Two Integers.go
@@ -0,0 +1,58 @@
+package leetcode
+
+import "math"
+
+// 两数相除
+// https://leetcode.cn/problems/divide-two-integers/
+
+// 暴力
+func divideForce(dividend, divisor int) int {
+	var sign, ans int
+	if (dividend^divisor)>>31&1 == 1 { // 除数被除数符号不同
+		sign = -1
+	} else { // 符号相同
+		sign = 1
+	}
+
+	// 根据除法定义，不停减去除数
+	for dividend >= divisor {
+		dividend -= divisor
+		ans++
+	}
+
+	ans *= sign
+	if ans > math.MaxInt64 || ans < math.MinInt64 {
+		return math.MaxInt64
+	}
+
+	return ans
+}
+
+// 指数递增步长
+func divide(dividend, divisor int) int {
+	var sign, ans int
+	if (dividend^divisor)>>31&1 == 1 { // 除数被除数符号不同
+		sign = -1
+	} else { // 符号相同
+		sign = 1
+	}
+
+	// 根据除法定义，不停减去除数
+	for dividend >= divisor {
+		i := 1
+		tmp := divisor
+		for dividend >= tmp {
+			dividend -= tmp
+			ans += i
+			i = i << 1
+			tmp = tmp << 1
+		}
+	}
+
+	ans *= sign
+	if ans > math.MaxInt64 || ans < math.MinInt64 {
+		return math.MaxInt64
+	}
+
+	return ans
+}
