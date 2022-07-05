1e5ba38ffaf04eb9275374cec5d360f820a6384b
Mr.Child <lishunsheng2012@gmail.com>
2022-07-05 22:36:38 +0800

    update comment

--- Golang/202. Happy Number.go
+++ Golang/202. Happy Number.go
@@ -12,13 +12,13 @@ func isHappy(n int) bool {
 	num := n
 	record := map[int]int{}
 	for {
-		for num != 0 {
+		for num != 0 { // 计算数字每一位的平方和
 			sum += (num % 10) * (num % 10)
 			num = num / 10
 		}
 
-		if _, ok := record[sum]; !ok {
-			if sum == 1 {
+		if _, ok := record[sum]; !ok { // 该数字没出现过，添加
+			if sum == 1 { // 平方和为1，该数是快乐数
 				return true
 			}
 
@@ -26,8 +26,27 @@ func isHappy(n int) bool {
 			num = sum
 			sum = 0
 			continue
-		} else {
+		} else { // 该数字出现过，无限循环，但始终变不到 1
 			return false
 		}
 	}
+}
+
+// 快慢指针
+func isHappyTwo(n int) bool {
+	slow, fast := n, step(n)
+	for fast != 1 && slow != fast {
+		slow = step(slow)
+		fast = step(step(fast))
+	}
+	return fast == 1
+}
+
+func step(n int) int {
+	sum := 0
+	for n > 0 {
+		sum += (n % 10) * (n % 10)
+		n = n / 10
+	}
+	return sum
 }
--- Golang/268. Missing Number.go
+++ Golang/268. Missing Number.go
@@ -4,18 +4,34 @@ import "sort"
 
 // 丢失的数字
 // https://leetcode.cn/problems/missing-number/
-// 只有一个数字丢失，返回丢失的数字
+// 在包含[0, n]共n个数的数组nums只有一个数字丢失，返回丢失的数字
 
 // 异或
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
 func missingNumber(nums []int) int {
 	xor, i := 0, 0
 	for i = 0; i < len(nums); i++ {
-		xor = xor ^ i ^ nums[i] // 除了丢失的数，每个数都出现2次
+		xor = xor ^ i ^ nums[i] // 除了丢失的数，每个数都出现2次，i^nums[i]==0
 	}
 	return xor ^ i
 }
 
+// 数学
+// 高斯公式
+func missingNumberMath(nums []int) int {
+	n := len(nums)
+	total := n * (n + 1) / 2
+	arrSum := 0
+	for _, num := range nums {
+		arrSum += num
+	}
+	return total - arrSum
+}
+
 // 排序
+// 时间复杂度：O(n log n)
+// 空间复杂度：O(log n)
 func missingNumberSort(nums []int) int {
 	sort.Ints(nums)
 	for i, num := range nums {
@@ -27,6 +43,8 @@ func missingNumberSort(nums []int) int {
 }
 
 // 哈希表
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
 func missingNumberMap(nums []int) int {
 	has := map[int]bool{}
 	for _, v := range nums {
